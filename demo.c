6fed984340d62cab0f4245582b63527afae53aaa
Manikanth Reddy <93320219+Manireddy69@users.noreply.github.com>
2025-05-31 19:18:26 +0530

    feels good

---
+++ demo.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("Concrete contains gravel & cement");
+    return 0;
+}
---
+++ first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int four;
+    // num = 1;
+
+    // printf("I am simple. \n");
+    // printf("Computer \n");
+    // printf("my favaritute number is %d because it is the first number.",num);
+    four = 4;
+    printf("%d\n",four);
+    
+    return 0;
+}
